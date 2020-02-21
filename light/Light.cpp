76a771b27343e525b138ee9d923d2efe986a51b2
hiru <rlawoehd187@naver.com>
2020-02-21 23:26:43 +0900

    ef56: Update lights HAL.

--- light/Light.cpp
+++ light/Light.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,43 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.ef56"
+#define LOG_TAG "LightService"
 
-#include <log/log.h>
-#include <fstream>
 #include "Light.h"
+
+#include <android-base/logging.h>
+
+namespace {
+using android::hardware::light::V2_0::LightState;
+
+static constexpr int RAMP_SIZE = 8;
+static constexpr int RAMP_STEP_DURATION = 50;
+
+static constexpr int BRIGHTNESS_RAMP[RAMP_SIZE] = {0, 12, 25, 37, 50, 72, 85, 100};
+static constexpr int DEFAULT_MAX_BRIGHTNESS = 255;
+
+static uint32_t rgbToBrightness(const LightState& state) {
+    uint32_t color = state.color & 0x00ffffff;
+    return ((77 * ((color >> 16) & 0xff)) + (150 * ((color >> 8) & 0xff)) +
+            (29 * (color & 0xff))) >> 8;
+}
+
+static bool isLit(const LightState& state) {
+    return (state.color & 0x00ffffff);
+}
+
+static std::string getScaledDutyPcts(int brightness) {
+    std::string buf, pad;
+
+    for (auto i : BRIGHTNESS_RAMP) {
+        buf += pad;
+        buf += std::to_string(i * brightness / 255);
+        pad = ",";
+    }
+
+    return buf;
+}
+}  // anonymous namespace
 
 namespace android {
 namespace hardware {
@@ -26,87 +58,65 @@ namespace light {
 namespace V2_0 {
 namespace implementation {
 
-#define LEDS                       "/sys/class/leds/"
-#define LCD_LED                    LEDS "lcd-backlight/"
-#define BUTTON_LED                 LEDS "gpled_0/"
-#define BUTTON1_LED                LEDS "gpled_1/"
-#define BUTTON2_LED                LEDS "gpled_2/"
-#define BUTTON3_LED                LEDS "gpled_3/"
-#define BRIGHTNESS                 "brightness"
-
-/*
- * Write value to path and close file.
- */
-static void set(std::string path, std::string value) {
-    std::ofstream file(path);
-    /* Only write brightness value if stream is open, alive & well */
-    if (file.is_open()) {
-        file << value;
-    } else {
-        /* Fire a warning a bail out */
-        ALOGE("failed to write %s to %s", value.c_str(), path.c_str());
-        return;
-    }
+Light::Light(std::pair<std::ofstream, uint32_t>&& lcd_backlight,
+             std::vector<std::ofstream>&& button_backlight)
+    : mLcdBacklight(std::move(lcd_backlight)),
+      mButtonBacklight(std::move(button_backlight)) {
+    auto backlightFn(std::bind(&Light::setLcdBacklight, this, std::placeholders::_1));
+    auto buttonsFn(std::bind(&Light::setButtonsBacklight, this, std::placeholders::_1));
+    mLights.emplace(std::make_pair(Type::BACKLIGHT, backlightFn));
+    mLights.emplace(std::make_pair(Type::BUTTONS, buttonsFn));
 }
 
-static void set(std::string path, int value) {
-    set(path, std::to_string(value));
-}
-
-/*
- * Scale each value of the brightness ramp according to the
- * brightness of the color.
- */
-
-static void handleBacklight(Type /*type*/, const LightState& state) {
-    uint32_t brightness = state.color & 0xFF;
-    brightness *= 16; // HACK
-    set(LCD_LED BRIGHTNESS, brightness);
-}
-
-static void handleButtons(Type /*type*/, const LightState& state) {
-    uint32_t brightness = state.color & 0xFF;
-    brightness *= 16; // HACK
-    set(BUTTON_LED BRIGHTNESS, brightness);
-    set(BUTTON1_LED BRIGHTNESS, brightness);
-    set(BUTTON2_LED BRIGHTNESS, brightness);
-    set(BUTTON3_LED BRIGHTNESS, brightness);
-}
-
-/*
- * Keep sorted in the order of importance.
- */
-static std::map<Type, std::function<void(Type type, const LightState&)>> lights = {
-    {Type::BACKLIGHT, handleBacklight},
-    {Type::BUTTONS, handleButtons},
-};
-
-Light::Light() {}
-
+// Methods from ::android::hardware::light::V2_0::ILight follow.
 Return<Status> Light::setLight(Type type, const LightState& state) {
-    auto it = lights.find(type);
+    auto it = mLights.find(type);
 
-    if (it == lights.end()) {
+    if (it == mLights.end()) {
         return Status::LIGHT_NOT_SUPPORTED;
     }
 
-    /*
-     * Lock global mutex until light state is updated.
-     */
+    it->second(state);
 
-    std::lock_guard<std::mutex> lock(globalLock);
-    it->second(type, state);
     return Status::SUCCESS;
 }
 
 Return<void> Light::getSupportedTypes(getSupportedTypes_cb _hidl_cb) {
     std::vector<Type> types;
 
-    for (auto const& light : lights) types.push_back(light.first);
+    for (auto const& light : mLights) {
+        types.push_back(light.first);
+    }
 
     _hidl_cb(types);
 
     return Void();
+}
+
+void Light::setLcdBacklight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+
+    uint32_t brightness = rgbToBrightness(state);
+
+    // If max panel brightness is not the default (255),
+    // apply linear scaling across the accepted range.
+    if (mLcdBacklight.second != DEFAULT_MAX_BRIGHTNESS) {
+        int old_brightness = brightness;
+        brightness = brightness * mLcdBacklight.second / DEFAULT_MAX_BRIGHTNESS;
+        LOG(VERBOSE) << "scaling brightness " << old_brightness << " => " << brightness;
+    }
+
+    mLcdBacklight.first << brightness << std::endl;
+}
+
+void Light::setButtonsBacklight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+
+    uint32_t brightness = rgbToBrightness(state);
+
+    for (auto& button : mButtonBacklight) {
+        button << brightness << std::endl;
+    }
 }
 
 }  // namespace implementation
